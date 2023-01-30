db75440da3182d5a8c0d3d02122554c84ddeaa2f
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-01-30 14:20:20 -0500

    set up project with sass styling

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-        working
-    </div>
-  );
-}
-
-export default App;
---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import '../styles/app.scss';
+
+export default function App() {
+    return (
+        <div>
+            working
+        </div>
+    )
+}
