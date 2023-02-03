e47584099918a31d234b04d1779565c976183808
sean stephen brian <seanstephenbrian@gmail.com>
2023-02-03 13:53:24 -0500

    develop header design

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 export default function Title() {
     return (
         <div className='title-section'>
             <div className='title-text'>
-                ArtCart
+                <Link to=''>ArtCart</Link>
             </div>
             <div className='subtitle-text'>
                 Buy the World's Most Expensive Art
