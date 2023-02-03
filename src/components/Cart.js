7eed45b273209bf1f2dab9083f687a0a49aff97b
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-02-03 15:40:00 -0500

    begin building logic of Cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Cart(props) {
 
     // props:
     const { hidden } = props;
 
+    // state:
+    const [cartItems, setCartItems] = useState([
+            {
+                id: 1,
+                title: 'test title',
+                artist: 'test artist',
+                price: 100000000
+            },
+            {
+                id: 30,
+                title: 'test title 2',
+                artist: 'test artist 2',
+                price: 50000000
+            }
+    ]);
+
+    const [cartTotal, setCartTotal] = useState(0);
+
+    // HOOKS:
+    // on initial render, check for existing cart in localStorage:
+    useEffect(() => {
+        if (localStorage.getItem('cartItems')) {
+            setCartItems(JSON.parse(localStorage.getItem('cartItems')));
+        }
+    }, []);
+
+    // whenever cartItems changes...
+    useEffect(() => {
+        // update cartTotal:
+        const total = cartItems.reduce((accumulator, currentItem) => accumulator + currentItem.price, 0);
+        setCartTotal(total);
+        // and save cart to local storage:
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    }, [cartItems]);
+
     // render conditions:
     if (hidden) {
         return;
     } else if (!hidden) {
         return (
-            <div>
-                cart
+            <div className='cart'>
+                <div className='cart-title'>
+                    Your ArtCart:
+                </div>
+                <div className='cart-items'>
+                    {cartItems.map((item) => {
+                        return (
+                            <div className='cart-item' key={item.id}>
+                                <div className='cart-item-details'>
+                                    {item.title}, {item.artist}, ${item.price}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className='cart-total'>
+                    Total: ${cartTotal}
+                </div>
+                
             </div>
         )
     }
